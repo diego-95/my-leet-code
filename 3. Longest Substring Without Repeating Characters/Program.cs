5d0e0b3e96a8acd8520911a018e650ae34994cbe
Diego <diego.polidori95@gmail.com>
2024-05-04 11:13:12 +0200

    Added Longest Substring Without Repeating Character

---
+++ 3. Longest Substring Without Repeating Characters/Program.cs
@@ -0,0 +1,58 @@
+﻿var example1 = new
+{
+    str = "abcabcbb",
+    output = 3
+};
+var example2 = new
+{
+    str = "bbbbb",
+    output = 1
+};
+var example3 = new
+{
+    str = "pwwkew",
+    output = 3
+};
+
+var solution = new Solution();
+
+var output = solution.LengthOfLongestSubstring(example1.str);
+Check(output, example1.output);
+output = solution.LengthOfLongestSubstring(example2.str);
+Check(output, example2.output);
+output = solution.LengthOfLongestSubstring(example3.str);
+Check(output, example3.output);
+
+Console.ReadLine();
+
+void Check(int output, int expected)
+{
+    if (output != expected)
+        throw new Exception("Error");
+}
+
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        var result = 0;
+        var dictionary = new Dictionary<char, char>();
+        var response = new List<char>();
+        for(int i = 0; i < s.Length; i++)
+        {
+            for(int j = i; j < s.Length; j++)
+            {
+                if (!dictionary.TryAdd(s[j], s[j])) break;
+            }
+            if (dictionary.Count > result)
+            {
+                result = dictionary.Count;
+                response = dictionary.Values.ToList();
+            }
+            dictionary.Clear();
+        }
+        response.ToList().ForEach(v => Console.Write(v));
+        Console.WriteLine();
+        return result;
+    }
+}
